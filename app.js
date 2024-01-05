237e0237ae501402561dd5743cb188507585ad87
TriMinhTrann <111098294+TriMinhTrann@users.noreply.github.com>
2024-01-06 02:31:06 +0900

    finish project

--- app.js
+++ app.js
@@ -6,3 +6,26 @@ function openMenu() {
    hamburgerBtn.classList.toggle("active");
 }
 
+const dropClicks = document.querySelectorAll(".dropClick");
+
+dropClicks.forEach(function(dropClick) {
+  dropClick.addEventListener('click', function() {
+      const btnDropDown = this.parentElement.querySelector('.btn-dropdown');
+   if (btnDropDown) {
+      btnDropDown.classList.toggle('btn-open');
+   } else {
+      console.log("No element with the class 'btn-dropdown' found in the parent.");
+   }
+   
+  });
+});
+
+
+
+
+
+
+
+
+
+
