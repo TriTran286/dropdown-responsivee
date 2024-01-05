4db07862e3908089ef05a1ce519bbd918f274c48
TriMinhTrann <111098294+TriMinhTrann@users.noreply.github.com>
2024-01-05 13:44:12 +0900

    Hamburger menu update

--- app.js
+++ app.js
@@ -1,21 +1,8 @@
 const hamburgerMenu = document.querySelector(".sidebar");
-const hamburgerBtn = document.querySelector(".openMenu");
+const hamburgerBtn = document.querySelector(".hamburger-menu");
 
 function openMenu() {
-   if (hamburgerMenu.style.display === "none" || !hamburgerMenu.style.display) {
-      hamburgerMenu.style.display = "flex";
-   } else {
-      hamburgerMenu.style.display = "none";
-   }
-}
-
-const btnDropdown = document.querySelector(".btn-dropdown")
-
-function buttonLSide() {
-   btnDropdown.classList.toggle('btn-open')
-}
-
-function buttonRSide() {
-   btnDropdown.classList.toggle('btn-open')
+   hamburgerMenu.classList.toggle("sidebar-active")
+   hamburgerBtn.classList.toggle("active");
 }
 
